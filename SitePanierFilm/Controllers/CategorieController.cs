f176e95715070236191062b4b0c4ea47098fcd1b
mrmeme <mrmeme>
2014-04-16 15:16:19 +0200

    Add link homepage

---
+++ SitePanierFilm/Controllers/CategorieController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SitePanierFilm.Models;
+
+namespace SitePanierFilm.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private site_marchandEntities db = new site_marchandEntities();
+
+        //
+        // GET: /Categorie/
+
+        public ActionResult Index()
+        {
+            return View(db.Category.ToList());
+        }
+
+        //
+        // GET: /Categorie/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        //
+        // GET: /Categorie/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Categorie/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Category.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        //
+        // GET: /Categorie/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        //
+        // POST: /Categorie/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //
+        // GET: /Categorie/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        //
+        // POST: /Categorie/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Category.Find(id);
+            db.Category.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
