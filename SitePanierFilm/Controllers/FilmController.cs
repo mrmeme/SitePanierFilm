4197c49ff3377e81fe8a7b27c64c4c3986c0149e
mrmeme <mrmeme>
2014-04-16 15:07:06 +0200

    Add FilmController

---
+++ SitePanierFilm/Controllers/FilmController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SitePanierFilm.Models;
+
+namespace SitePanierFilm.Controllers
+{
+    public class FilmController : Controller
+    {
+        private site_marchandEntities db = new site_marchandEntities();
+
+        //
+        // GET: /Film/
+
+        public ActionResult Index()
+        {
+            var film = db.Film.Include(f => f.Category);
+            return View(film.ToList());
+        }
+
+        //
+        // GET: /Film/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Film film = db.Film.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        //
+        // GET: /Film/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Category_id = new SelectList(db.Category, "Id", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Film/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Film.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Category_id = new SelectList(db.Category, "Id", "Name", film.Category_id);
+            return View(film);
+        }
+
+        //
+        // GET: /Film/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Film film = db.Film.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Category_id = new SelectList(db.Category, "Id", "Name", film.Category_id);
+            return View(film);
+        }
+
+        //
+        // POST: /Film/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(film).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category_id = new SelectList(db.Category, "Id", "Name", film.Category_id);
+            return View(film);
+        }
+
+        //
+        // GET: /Film/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Film film = db.Film.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        //
+        // POST: /Film/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Film film = db.Film.Find(id);
+            db.Film.Remove(film);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SitePanierFilm/Models/ModelFilm.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'c:\users\noel\documents\visual studio 2012\Projects\SitePanierFilm\SitePanierFilm\Models\ModelFilm.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
